b4f84c8c5c43f4eb0f31e37751bfb86dde812f44
johnnytortorici <johnny.tortorici@gmail.com>
2020-08-06 17:01:36 -0400

    Exercise 4

---
+++ src/reducers/artists-reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  currentArtist: null,
+  status: "idle",
+};
+
+export default function artistReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
